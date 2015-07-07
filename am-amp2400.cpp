1aa920fbd232c9484a19354a1b91b8b0e9637c51
Ansel <aesthapus@gmail.com>
2015-07-07 11:45:09 -0400

    add copyright info to top of the source files and remove unneeded includes (QtGui)

--- am-amp2400.cpp
+++ am-amp2400.cpp
@@ -1,3 +1,20 @@
+/*
+	Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include "am-amp2400.h"
 
@@ -74,7 +91,7 @@ AMAmp::~AMAmp(void) {};
 
 void AMAmp::initParameters(void) {
 	input_channel = 0;
-	output_channel = 1;
+	output_channel = 0;
 	amp_mode = 2;
 
 	device = 0;
